ae9b3587fca6145b4e89e27fb1508d7e8a4ed8c8
danaele <danaele@email.unc.edu>
2016-02-10 16:55:58 -0500

    Read list matrix path (text file) et then read each matrix avec vector<vector<>>

--- matricesStat.cxx
+++ matricesStat.cxx
@@ -1,16 +1,151 @@
 #include "matricesStat.h"
 
+std::vector< std::vector<float> >read_probtrackx2_matrix( std::string inputMatrixTextFile )
+{
+    std::string fileName = inputMatrixTextFile;
+    std::ifstream inputFile ;
+    inputFile.open( fileName.c_str() , std::ios::in ) ;
+
+    std::vector< std::string > mat_string;
+    std::vector< std::string >::const_iterator mat_it,mat_end;
+    std::string labelLine;
+
+    //Catch each row of the matrix in a vector of string
+    if(inputFile.good())
+    {
+        do
+        {
+            getline( inputFile , labelLine ) ; //get information line
+        }while( labelLine[0] == '#' ) ;
+
+        while(!inputFile.eof())
+        {
+            if(labelLine.empty() !=true )
+            {
+                mat_string.push_back(labelLine);
+            }
+            getline(inputFile,labelLine);
+        }
+    }
+    inputFile.close();
+
+    //Number of row
+    int nb_lines=mat_string.size();
+    std::cout<<"Number of rows : "<<nb_lines<<std::endl;
+
+    std::vector< std::vector <std::string> > mat;
+    std::string::const_iterator s_it, s_end ;
+
+    std::vector< std::vector<float> > mat_num;
+
+    //Find each matrix components
+    for(mat_it=mat_string.begin() , mat_end=mat_string.end() ; mat_it!=mat_end ; ++mat_it)
+    {
+        std::string line = *mat_it;
+        std::vector< std::string > cells;
+        std::string number;
+
+        for(s_it=line.begin() , s_end=line.end() ; s_it!=s_end ; ++s_it)
+        {
+            if(*s_it != ' ')
+            {
+                number += *s_it ;
+                if(s_it == s_end-1)  //if last character is a number
+                {
+                    cells.push_back(number);
+                    number.clear();
+                }
+            }
+            else
+            {
+                if( number != "")
+                {
+                    cells.push_back(number);
+                    number.clear();
+                }
+                else
+                {
+                    //Catch next char
+                }
+            }
+        }
+        //Verified that is a square matrix
+        if(cells.size() != nb_lines)
+        {
+            std::cout<<"Error : dimension of matrix not matched - Matrix should be square "<<std::endl;
+            mat_num.clear();
+            return mat_num;
+        }
+        else
+        {
+            mat.push_back(cells);
+
+        }
+        cells.clear();
+    }
+    std::cout<<"Matrix dimension verified "<<nb_lines<<"x"<<nb_lines<<std::endl;
+
+
+    //Convert matrix of string with matrix of double
+    for(int i = 0 ; i < nb_lines ; i++)
+    {
+        std::vector<float>  row_num;
+        for(int j = 0 ; j < nb_lines ; j++)
+        {
+            std::string component = mat[i][j];
+            double num_component = atoi( component.c_str() );
+            row_num.push_back(num_component);
+
+        }
+        mat_num.push_back(row_num);
+    }
+    return mat_num;
+}
+
+
 
 int main ( int argc, char *argv[] )
 {
   PARSE_ARGS ;
   
-  std::cout<<"Hello Stat "<<std::endl;
-  
-  
-  
-  
-  
+  std::ifstream inputFile ;
+  std::string labelLine;
+
+  std::vector<std::string> listMatPath;
+  std::vector<std::string>::const_iterator lit, lend;
+  inputFile.open( listMatrixPath.c_str() , std::ios::in );
+  if(inputFile.good())
+  {
+      while(!inputFile.eof())
+      {
+        getline( inputFile , labelLine ) ;
+        //std::cout<<labelLine<<std::endl;
+        if(!labelLine.empty())
+        {
+            listMatPath.push_back(labelLine);
+        }
+      }
+
+  }
+  inputFile.close();
+
+  //ReadMatrixList
+
+  std::list < std::vector< std::vector<float> > > listMatrix;
+  int n=0;
+
+  for(lit=listMatPath.begin(), lend=listMatPath.end() ; lit!= lend ; lit++)
+  {
+
+      std::cout<<*lit<<std::endl;
+      std::vector< std::vector<float> > matrix;
+      matrix=read_probtrackx2_matrix(*lit);
+      listMatrix.push_back(matrix);
+      n += 1;
+
+  }
+    std::cout<<n<<std::endl;
+
   return 0;
   }
 
--- matricesStat.h
+++ matricesStat.h
@@ -1,2 +1,11 @@
 #include <cstdio> 
 #include <matricesStatCLP.h>
+#include <itkDirectory.h>
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <stdlib.h>
+#include <vector>
+#include <list>
+
+std::vector< std::vector<float> > read_probtrackx2_matrix( std::string inputMatrixTextFile );
